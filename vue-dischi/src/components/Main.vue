3737d23246e15adc2cd584994665875ca228f0c9
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-01-18 15:08:24 +0100

    search album

--- vue-dischi/src/components/Main.vue
+++ vue-dischi/src/components/Main.vue
@@ -1,12 +1,21 @@
 
 <template>
   <div class="container bg-main-color">
+      <div class="row justify-content-center pt-3">
+        <select name="cars" id="cars" class="w-25" @change="filteredAlbum($event)" v-model="key">
+          <option value ="Tutti"> Tutti</option>
+          <option value ="Rock"> Rock</option>
+          <option value ="Pop"> Pop</option>
+          <option value ="Metal"> Metal</option>
+          <option value ="Jazz"> Jazz</option>
+        </select>
+      </div>
       <div class="row container-cards row-cols-5">
         <div v-for="(album, index) in cards" :key="index" class="col p-3">
           <img class="mb-2" :src="album.poster" :alt="album.title">
           <h2 class="text-light mb-3 text-uppercase fs-4">{{ album.title }}</h2>
-          
         </div>
+  
 
       </div>
   </div>
@@ -19,17 +28,36 @@ export default {
   
   data() {
       return {
-          cards: null
+          cards: null,
+          key: "" ,
+          filteredcards: null,
+          all:'',
+
       }
     },
     created () {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(response => {
               this.cards = response.data.response;
+              this.all = response.data.response;
+              this.filteredcards = response.data.response;
               console.log(this.cards)
             })
             
+    },
+    methods: {
+        filteredAlbum(event) {
+          if (event.target.value === 'Tutti') {
+            this.cards = this.all
+           return this.cards;
+      } else {
+        return this.cards = this.filteredcards.filter((element) => element.genre.includes(event.target.value));
+      }
+          
+           
     }
+    }
+    
     
 }
 </script>
@@ -39,7 +67,6 @@ export default {
     .bg-main-color {
         background-color: #1E2D3B;
         height: 100vh;
-        display: flex;
         align-items: center;
         justify-content: center;
     }
